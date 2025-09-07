a70db84aa232e43b8c29fba67a5af994205e35a5
rahul-pandey-20330 <rahulpandeyr184@gmail.com>
2025-09-07 23:09:48 +0530

    Real Time Tracker Project is completed

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const path = require('path')
+const http = require("http")
+
+const socketio = require("socket.io"); //socketio runs on http server
+const server = http.createServer(app);
+const io = socketio(server);
+app.set("view engine", 'ejs');
+app.use(express.static(path.join(__dirname, "public")));
+io.on("connection",function(socket){
+    socket.on("send-location",(data)=>{
+        io.emit("receive-location",{id: socket.id, ...data});
+    });
+   
+    socket.on("disconnect",()=>{
+        io.emit("user-disconnected,socket.id")
+    })
+    
+})
+app.get("/",(req,res)=>{
+    res.render('index')
+})
+server.listen(3000);
