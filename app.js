151492a1f4e0561662c7300f06c6d51f6499bbfe
rahul-pandey-20330 <rahulpandeyr184@gmail.com>
2025-09-08 00:05:32 +0530

    name me apna naam htaya and Mitro Likha hai

--- app.js
+++ app.js
@@ -21,4 +21,7 @@ io.on("connection",function(socket){
 app.get("/",(req,res)=>{
     res.render('index')
 })
-server.listen(3000);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
